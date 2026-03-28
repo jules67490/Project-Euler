eb88a06b8069ebe6f4c56e87d7f9f6054f03dd88
Jules Winstel <jules.winstel24@gmail.com>
2026-03-28 14:10:52 +0100

    Ex 5 Done

--- Ex5.py
+++ Ex5.py
@@ -1,7 +1,11 @@
-def fact(n):
-    if n == 1:
-        return 1
-    else:
-        return n*fact(n-1)
-    
-print(fact(20))
+def Ex5():
+    '''This can be done by hand. We need to multiply:
+    - The prime numbers between 1 and 20: 2, 3, 5, 7, 11, 13, 17, 19 (8 numbers already)
+    - By multiplying these numbers, we already have 6, 10, 14, 15 (+ 4 = 12)
+    - An additional 2 to have 4, 12 and 20 (+ 3 = 15)
+    - An additional 3 to have 9 and 18 (+ 2 = 17)
+    - An additional 2 to have 8 (+ 1 = 18). 
+    - An additional 2 to have 16 (+ 1 = 19). Adding 1, and number created by the multiplication of all the previous ones will be the smallest evenly divisible by all number from 1 to 20'''
+    return 2*2*2*2*3*3*5*7*11*13*17*19
+
+print(Ex5())
